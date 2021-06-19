15fdac696f29f58bd4a65570e0a81949a2bdecfc
Abhishek Singh <abhisheksingh1911a5@gmail.com>
2021-06-19 17:19:20 +0530

    lab pgm 1-3

--- src/com/company/pgm3b.java
+++ src/com/company/pgm3b.java
@@ -1,5 +1,9 @@
 package com.company;
-
+/*
+3b. Write a Java program that implements a multi-thread application that has three threads. First thread
+generates a random integer for every 1 second; second thread computes the square of the number and
+prints; third thread will print the value of cube of the numbers.
+ */
 import java.util.Random;
 public class pgm3b {
     static int randomInt;
